3afcd11b7f18ab2e45c9229a19d42b14b18c5b04
Jill Short <jillian.b.short@gmail.com>
2020-07-10 03:29:37 +0000

    Automatically backed up by Learn

--- lib/while.rb
+++ lib/while.rb
@@ -1,6 +1,6 @@
 def using_while (using_while="Wingardium Leviosa")
   levitation_force = 6
-    while levitation_force < 10
+    while levitation_force < 11
 	  levitation_force += 1
 	  puts "#{using_while}"
 	end
