d16dfc669d7ea5c88f6d14a1a4e21a77b7cb1bb0
Jill Short <jillian.b.short@gmail.com>
2020-07-10 03:44:15 +0000

    Automatically backed up by Learn

--- lib/while.rb
+++ lib/while.rb
@@ -7,7 +7,3 @@ def using_while (phrase="Wingardium Leviosa")
 end
 
 
-# while num_of_hotdogs_eaten < 7
-#  num_of_hotdogs_eaten += 1
-#  puts "You have now eaten #{num_of_hotdogs_eaten} hot dog(s)."
-#end
